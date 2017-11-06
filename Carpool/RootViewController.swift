2c6dd4478663dabb524b8f2fad96375ce2be4bdb
Akash Shah <Aimee@akashs-macbook-pro.local>
2017-11-06 15:28:22 -0500

    EventPage WIP

--- Carpool/RootViewController.swift
+++ Carpool/RootViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class RootViewController: UITableViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
