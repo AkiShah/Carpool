0405f63fe2d11ee428bb6b4c569265fb2fe3034d
Zaller <Zaller@UBCs-MacBook-Pro.local>
2017-11-06 15:24:25 -0500

    Getting tables together, adding variables from model.
    Still stuck on not getting the cell to turn red when selected, and not able to display the events yet.

--- Carpool/RootViewController.swift
+++ Carpool/RootViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 import CarpoolKit
+import CoreLocation
 
 class RootViewController: UITableViewController {
-
+    
+    var trips: [Trip] = []
+    var events: [Event] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        API.fetchTripsOnce { (givenTrips) in
+            self.trips = givenTrips
+            self.tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "eventList", for: indexPath)
+        
+        //this is not working yet to turn it red.
+        if cell.isSelected == true {
+            cell.backgroundColor = UIColor.red
+        }
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+        
+    }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
