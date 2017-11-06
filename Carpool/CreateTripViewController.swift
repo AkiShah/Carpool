88ba1a59e59c9f2e8380e9d2c2f68024f6ac5b3e
Zaller <Zaller@UBCs-MacBook-Pro.local>
2017-11-06 17:10:07 -0500

    Created a create trip VC and file.

---
+++ Carpool/CreateTripViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTripViewController.swift
+//  Carpool
+//
+//  Created by Zaller on 11/6/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import Foundation
+import CarpoolKit
+import CoreLocation
+
+class CreateTripViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
