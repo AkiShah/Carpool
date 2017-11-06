2745c412f6099b0286ab545a6956a0ef2004273e
Akash Shah <Aimee@akashs-macbook-pro.local>
2017-11-06 14:28:10 -0500

    Sprint 1 Setup
    * List all Events in a UITableViewController called `RootViewController`
    * Tapping an event shows all available information in an `EventDetailViewController`
    * Mark unclaimed legs with red bg
    * Tapping an unclaimed cell allows the user to claim it (alertView)

---
+++ Carpool/EventDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailViewController.swift
+//  Carpool
+//
+//  Created by Akash Shah on 11/6/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Carpool/RootViewController.swift
+++ Carpool/RootViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
