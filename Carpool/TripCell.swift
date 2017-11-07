e69876a94104dc2c643cabd0411f200d71206188
Zaller <Zaller@UBCs-MacBook-Pro.local>
2017-11-07 12:29:16 -0500

    created pickup drop-off and description labels. and a new cell file.

---
+++ Carpool/TripCell.swift
@@ -0,0 +1,18 @@
+//
+//  TripCell.swift
+//  Carpool
+//
+//  Created by Zaller on 11/7/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class TripCell: UITableViewCell {
+    
+    @IBOutlet weak var pickupLabel: UILabel!
+    @IBOutlet weak var dropoffLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
