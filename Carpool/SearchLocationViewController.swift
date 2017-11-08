0221da24e14752c5efc5908868f21bd966b0c6fe
Zaller <Zaller@UBCs-MacBook-Pro.local>
2017-11-08 11:40:27 -0500

    added a mapKit and map vc.

---
+++ Carpool/SearchLocationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchLocationViewController.swift
+//  Carpool
+//
+//  Created by Zaller on 11/8/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SearchLocationViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
+
