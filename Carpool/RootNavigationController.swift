f9eb4a63c7cb3c3f02c0bb8f65c499af6a6bae12
Unknown <thekuan@gmail.com>
2017-11-09 11:47:38 -0500

    New RootNC and updated cells

---
+++ Carpool/RootNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  RootNavigationController.swift
+//  Carpool
+//
+//  Created by Zaller on 11/9/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootNavigationController: UINavigationController {
+    
+}
