bf3da11b077601196cc9fa2e2bf7758ccbe8c9b8
Unknown <thekuan@gmail.com>
2017-11-10 12:24:31 -0500

    Added a friends VC and connected it through nav controller

---
+++ Carpool/FriendsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FriendsViewController.swift
+//  Carpool
+//
+//  Created by Zaller on 11/10/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+import CarpoolKit
+
+class FriendsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
