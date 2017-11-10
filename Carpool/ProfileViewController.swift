9232f00f694bda3cc8821ad42cd4a8cc2d70d093
Unknown <thekuan@gmail.com>
2017-11-10 16:31:50 -0500

    Pushing. Pushing real good.

--- Carpool/ProfileViewController.swift
+++ Carpool/ProfileViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 import CarpoolKit
+import FirebaseCommunity
 
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(forName: logMeInNotificationName, object: nil, queue: .main) { _ in
+            if let loginVC = self.presentedViewController as? LoginViewController {
+                loginVC.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let currentUser = Auth.auth().currentUser {
+            print(currentUser)
+            NotificationCenter.default.post(name: logMeInNotificationName, object: nil)
+        } else {
+            let loginVC = storyboard!.instantiateViewController(withIdentifier: "LoginVC")
+            present(loginVC, animated: animated, completion: nil)
+        }
     }
 
     @IBOutlet weak var userNameLabel: UILabel!
