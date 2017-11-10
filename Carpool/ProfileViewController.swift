8d7b82f9bdd0f446e95dfd6e1c8499dda44422d7
Unknown <thekuan@gmail.com>
2017-11-10 16:48:32 -0500

    Working on profile VC

--- Carpool/ProfileViewController.swift
+++ Carpool/ProfileViewController.swift
@@ -8,39 +8,37 @@
 
 import UIKit
 import CarpoolKit
-import FirebaseCommunity
 
 class ProfileViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(forName: logMeInNotificationName, object: nil, queue: .main) { _ in
-            if let loginVC = self.presentedViewController as? LoginViewController {
-                loginVC.dismiss(animated: true, completion: nil)
-            }
-        }
-    }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if let currentUser = Auth.auth().currentUser {
-            print(currentUser)
-            NotificationCenter.default.post(name: logMeInNotificationName, object: nil)
-        } else {
-            let loginVC = storyboard!.instantiateViewController(withIdentifier: "LoginVC")
-            present(loginVC, animated: animated, completion: nil)
-        }
-    }
-
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var partnerNameLabel: UILabel!
-    @IBAction func onEditProfilePressed(_ sender: UIButton) {
-    }
     @IBOutlet weak var onUserNameEdited: UITextField!
     @IBOutlet weak var onPartnerNameEdited: UITextField!
     @IBOutlet weak var onChildNameAdded: UITextField!
     @IBOutlet weak var onSignOutPressed: UIButton!
     @IBOutlet weak var childTableView: UITableView!
     
+    var user: User?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        API.fetchCurrentUser { result in
+            switch result {
+                
+            case .success(let user):
+                self.user = user
+                self.userNameLabel.text = user.name
+            case .failure(_):
+                break//todo error
+            }
+        }
+        
+        onUserNameEdited.isHidden = true
+        onPartnerNameEdited.isHidden = true
+        
+    }
+    
+    @IBAction func onEditProfilePressed(_ sender: UIButton) {
+    }
 }
