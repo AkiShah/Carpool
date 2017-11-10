a303f198f9c66c5c75cc1510134c6b21025de3a4
Akash Shah <akashshah7@outlook.com>
2017-11-10 12:15:18 -0500

    Buttons go switch switch when you click claim or unclaim. Waiting on observe trip to make it work

--- Carpool/TripDetailViewController.swift
+++ Carpool/TripDetailViewController.swift
@@ -23,34 +23,43 @@ class TripDetailViewController: UIViewController {
     
     var trip: Trip!
     
+    enum TripLeg {
+        case dropoff
+        case pickup
+    }
+    
+    enum isLegClaimed {
+        case claimed
+        case unclaimed
+
+        var rawValue: Bool {
+            switch self {
+            case .claimed:
+                return true
+            case .unclaimed:
+                return false
+            }
+        }
+        init (tripLeg: Leg?) {
+            self = tripLeg != nil ? .claimed : .unclaimed
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         eventDescriptionLabel.text = trip.event.description
         
-        if let leg = trip.pickUp {
-            pickupButton.isEnabled = false
-            pickupButton.setTitle("Pickup Claimed", for: .normal)
-            eventPickupDriverLabel.text = leg.driver.name
-        } else {
-            pickupButton.isEnabled = true
-            pickupButton.setTitle("Claim Pickup", for: .normal)
-        }
-        if let leg = trip.dropOff {
-            dropoffButton.isEnabled = false
-            dropoffButton.setTitle("Drop Off Claimed", for: .normal)
-            eventDropoffDriverLabel.text = leg.driver.name
-        } else {
-            dropoffButton.isEnabled = true
-            dropoffButton.setTitle("Claim Drop Off", for: .normal)
-        }
+        updateButtonState(for: .dropoff)
+        updateButtonState(for: .pickup)
         
         let geocoder = CLGeocoder()
         if let location = trip.event.clLocation {
             geocoder.reverseGeocodeLocation(location) { placemarks, error in
                 if let state = placemarks?.first?.administrativeArea, let city = placemarks?.first?.locality{
                     self.eventLocationLabel.text = "\(city), \(state)"
-                } else {
+                } else if let error = error {
                     self.errorMessages(error: error)
                 }
             }
@@ -63,52 +72,106 @@ class TripDetailViewController: UIViewController {
         
     }
 
+    //TODO Give button a selected and unselected case to simplify further
+    
+    func updateButtonState(for leg: TripLeg) {
+        let claimStatus = isLegClaimed(tripLeg: leg == .dropoff ? trip.dropOff : trip.pickUp)
+        
+        switch (leg, claimStatus) {
+        case (.dropoff, .claimed):
+            let driver = trip.dropOff!.driver
+            API.fetchCurrentUser(completion: { result in
+                switch result {
+                case .success(let user):
+                    print("Neer mind, you can haz this leg back")
+                    self.dropoffButton.isEnabled = user == driver
+                case .failure:
+                    //TODO Error Handling
+                    break
+                }
+            })
+            dropoffButton.setTitle("Drop Off Claimed", for: .normal)
+            eventDropoffDriverLabel.text = driver.name
+        case (.dropoff, .unclaimed):
+            dropoffButton.isEnabled = true
+            print("I hereby claim this leg as mine")
+            dropoffButton.setTitle("Claim Drop Off", for: .normal)
+        case (.pickup, .claimed):
+            let driver = trip.pickUp!.driver
+            API.fetchCurrentUser(completion: { result in
+                switch result {
+                case .success(let user):
+                    print("Neer mind, you can haz this leg back")
+                    self.pickupButton.isEnabled = user == driver
+                case .failure:
+                    //TODO Error Handling
+                    break
+                }
+            })
+            pickupButton.setTitle("Pickup Claimed", for: .normal)
+            eventPickupDriverLabel.text = driver.name
+        case (.pickup, .unclaimed):
+            pickupButton.isEnabled = true
+            print("I hereby claim this leg as mine")
+            pickupButton.setTitle("Claim Pickup", for: .normal)
+        }
+    }
+    
+    @IBAction func onDropoffButtonPressed(_ sender: UIButton) {
+        switch isLegClaimed(tripLeg: trip.dropOff) {
+            
+        case .claimed:
+            API.unclaimPickUp(trip: trip, completion: pickUpClaimChanged)
+        case .unclaimed:
+            let alert = UIAlertController(title: "Claim this Dropoff", message: "Would you like to claim this dropoff?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Claim Dropoff", style: .default, handler: {_ in
+                API.claimDropOff(trip: self.trip, completion: self.dropOffClaimChanged)
+            }))
+            alert.addAction(UIAlertAction(title: "Not Now", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     @IBAction func onPickupButtonPressed(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Claim this Pickup", message: "Would you like to claim this pickup?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Claim Pickup", style: .default, handler: { _ in
-            
-            API.claimPickUp(trip: self.trip, completion: { error in
-                if let error = error{
-                    print(error)
-                    self.errorMessages(error: error)
-                } else {
-                    self.eventPickupDriverLabel.text = "Aki"
-                    self.pickupButton.isEnabled = false
-                }
-            })
-        }))
-        alert.addAction(UIAlertAction(title: "Not Now", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
         
-    }
-    @IBAction func onDropoffButtonPressed(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Claim this Dropoff", message: "Would you like to claim this dropoff?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Claim Dropoff", style: .default, handler: {_ in
-            API.claimDropOff(trip: self.trip, completion: { error in
-                if let error = error{
-                    print(error)
-                    self.errorMessages(error: error)
-                } else {
-                    self.eventDropoffDriverLabel.text = "Shannon"
-                    self.dropoffButton.isEnabled = false
-                }
-            })
-            
-        }))
-        alert.addAction(UIAlertAction(title: "Not Now", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        switch isLegClaimed(tripLeg: trip.pickUp) {
+        case .claimed:
+            API.unclaimPickUp(trip: trip, completion: dropOffClaimChanged)
+        case .unclaimed:
+            let alert = UIAlertController(title: "Claim this Pickup", message: "Would you like to claim this pickup?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Claim Pickup", style: .default, handler: { _ in
+                API.claimPickUp(trip: self.trip, completion: self.dropOffClaimChanged)
+            }))
+            alert.addAction(UIAlertAction(title: "Not Now", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
-    func errorMessages(error: Error?) {
-        if let msg = error?.localizedDescription {
-            let alert = UIAlertController(title: "Whoops", message: msg, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "I'll try again", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-
+    func dropOffClaimChanged(error: Error?) {
+        if let error = error {
+            errorMessages(error: error)
         } else {
-            title = "OOOOHH NOOOOO"
+            print("Look mom I made a change")
+            updateButtonState(for: .dropoff)
         }
+        
+    }
+    
+    
+    func pickUpClaimChanged(error: Error?) {
+        if let error = error {
+            errorMessages(error: error)
+        } else {
+            print("Look mom I made a change")
+            updateButtonState(for: .pickup)
+        }
+        
+    }
+    
+    func errorMessages(error: Error) {
+        let alert = UIAlertController(title: "Whoops", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "I'll try again", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
