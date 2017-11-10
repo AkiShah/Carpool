4675081a3bb553e2ad21f475ab71321bd4f2a735
Akash Shah <akashshah7@outlook.com>
2017-11-10 15:50:20 -0500

    programmatically added search bar

--- Carpool/FriendsViewController.swift
+++ Carpool/FriendsViewController.swift
@@ -11,9 +11,36 @@ import CarpoolKit
 import ContactsUI
 
 class FriendsViewController: UIViewController, CNContactPickerDelegate {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     @IBOutlet weak var searchBar: UISearchBar!
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = true
+        searchController.searchBar.placeholder = "What's that parent's name again?"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+    }
+    
+}
+
+extension FriendsViewController: UISearchBarDelegate {
+    
+}
+
+extension FriendsViewController: UISearchControllerDelegate {
+
+}
+
+extension FriendsViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+    
+
 }
