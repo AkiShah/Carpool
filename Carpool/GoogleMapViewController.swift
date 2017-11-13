959713462d596612e8f5e1667e5b1523b1cc2a70
Zaller <thekuan@gmail.com>
2017-11-13 11:55:21 -0500

    Created Google Storyboard and VC file so we can play with Storyboard
    without having to commit to using it. If you like it, we can use it at the end of the week. We can work on this part of the storyboard without worrying about someone else being in the main storyboard and fucking it up. This is what Nathan walked through this morning.

---
+++ Carpool/GoogleMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleMapViewController.swift
+//  Carpool
+//
+//  Created by Zaller on 11/13/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+
+class GoogleMapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
