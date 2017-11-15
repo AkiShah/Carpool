d6417909232b79a9e9b4bd225deca208431c8be0
Akash Shah <akashshah7@outlook.com>
2017-11-15 12:16:50 -0500

    Added the things for collectionView in header of rootvc

---
+++ Carpool/CalendarCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarCell.swift
+//  Carpool
+//
+//  Created by Akash Shah on 11/15/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionWeekdayLabel: UILabel!
+    @IBOutlet weak var collectionDateLabel: UILabel!
+    @IBOutlet weak var collectionScheduledEventsIndicator: UIView!
+    
+}
+
+extension RootViewController {
+    //This is where all the collectionview magic happens for the calendar
+    
+    
+}
