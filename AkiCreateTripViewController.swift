f7117205b4631f985be0fc0992abc9b70459e769
Akash Shah <akashshah7@outlook.com>
2017-11-16 08:45:49 -0500

    changing to right date picker

--- AkiCreateTripViewController.swift
+++ AkiCreateTripViewController.swift
@@ -8,6 +8,52 @@
 import Foundation
 import UIKit
 
+
 class AkiCreateTripViewController: UIViewController {
+    
+    var destination: String = ""
+    //Location
+    var date: Date =  Date()
+    var startTime: Date =  Date()
+    var endTime: Date =  Date()
+    var repeatTrip: Bool = false
+    var kidsOnTrip: [Child] = []
+    var selectedButton: DayOrTime = .disabled
+    enum DayOrTime {
+        case day
+        case startTime
+        case endTime
+        case disabled
+    }
+    
+    func changeDatePicker(to newSelection: DayOrTime) {
+        switch newSelection {
+        case day:
+            //switch to day picker
+            break
+        case startTime, endTime:
+            break
+            //switch to time picker
+        case disabled:
+            //hide datepicker
+            break
+        }
+    }
+    
+    func saveDatetTo() {
+        switch selectedButton{
+        case day:
+            //update correct button title
+            date = Date()
+        case startTime:
+            //update correct button title
+            startTime = Date()
+        case endTime:
+            //update correct button title
+            endTime = Date()
+        case disabled:
+            break
+        }
+    }
     
 }
