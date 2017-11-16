39a94977208ce768506d4e5fe9be0f6a9bb2832f
Zaller <thekuan@gmail.com>
2017-11-16 11:26:31 -0500

    changing more colors on the home page

--- Carpool/HomeScreenViewController.swift
+++ Carpool/HomeScreenViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+    @IBOutlet weak var viewTripsButton: UIButton!
+    @IBOutlet weak var addATripButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewTripsButton.backgroundColor = UIColor.init(red: 0.29, green: 0.31, blue: 0.40, alpha: 1.0)
+        addATripButton.backgroundColor = UIColor.init(red: 0.91, green: 0.76, blue: 0.51, alpha: 1.0)
+        
     }
 }
