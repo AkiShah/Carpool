924277e5a36fe1887102d7f3c4e15ce2e54785c2
Akash Shah <akashshah7@outlook.com>
2017-11-16 06:19:06 -0500

    Added new storyboard to work on new createTripVC

---
+++ AkiCreateTripViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AkiCreateTripViewController.swift
+//  
+//
+//  Created by Akash Shah on 11/16/17.
+//
+
+import Foundation
+import UIKit
+
+class AkiCreateTripViewController: UIViewController {
+    
+}
