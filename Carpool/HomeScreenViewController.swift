7a30a6a190e31e4f710c34803f4f5396d5d24339
Zaller <thekuan@gmail.com>
2017-11-17 00:34:06 -0500

    Added an outlet for name to the home vc and updated button pics.

--- Carpool/HomeScreenViewController.swift
+++ Carpool/HomeScreenViewController.swift
@@ -7,26 +7,29 @@
 //
 
 import UIKit
+import CarpoolKit
 
 class HomeScreenViewController: UIViewController {
     @IBOutlet weak var viewTripsButton: UIButton!
     @IBOutlet weak var addATripButton: UIButton!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Need to add a name
         viewTripsButton.backgroundColor = UIColor.init(red: 0.29, green: 0.31, blue: 0.40, alpha: 1.0)
         viewTripsButton.layer.borderWidth = 2
         viewTripsButton.layer.borderColor = UIColor.init(red: 0.29, green: 0.31, blue: 0.40, alpha: 1.0).cgColor
         addATripButton.backgroundColor = UIColor.init(red: 0.91, green: 0.76, blue: 0.51, alpha: 1.0)
         addATripButton.layer.borderWidth = 2
         addATripButton.layer.borderColor = UIColor.init(red: 0.29, green: 0.31, blue: 0.40, alpha: 1.0).cgColor
-        //self.addATripButton.roundCorners(corners: [.topLeft, .topRight], radius: 10)
-        
-        
         
     }
 }
+
+
 
 //extension UIButton
 //{
